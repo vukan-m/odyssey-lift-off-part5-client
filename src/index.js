d37e144678393485977966fab7082d76da2b0225
vukanm <vukan.mirkovic@gmail.com>
2022-08-02 07:19:22 +0200

    Uri change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://apollo-odyssey-app.herokuapp.com/", // change to YOUR own production server
+  uri: "https://apollo-odyssey-app-server.herokuapp.com/", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
